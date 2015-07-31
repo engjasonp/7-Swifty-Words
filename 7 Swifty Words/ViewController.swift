a154c55d2c06f029989f0cf22c158adab163e3f2
engjasonp <engjasonp@gmail.com>
2015-07-30 19:19:49 -0700

    Added Array+Shuffling.swift. Created loop to go through each button tagged "1001" and appended it to letterButtons array.

--- 7 Swifty Words/ViewController.swift
+++ 7 Swifty Words/ViewController.swift
@@ -14,14 +14,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var answersLabel: UILabel!
     @IBOutlet weak var currentAnswer: UITextField!
     @IBOutlet weak var scoreLabel: UILabel!
+    
+    var letterButtons = [UIButton]()
+    var activatedButtons = [UIButton]()
+    var solutions = [String]()
+    
+    var score = 0
+    var level = 1
+    
     @IBAction func submitTapped(sender: UIButton) {
+        
     }
     @IBAction func clearTapped(sender: UIButton) {
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        for subview in view.subviews {
+            if subview.tag == 1001 {
+                let btn = subview as! UIButton
+                letterButtons.append(btn)
+                btn.addTarget(self, action: "letterTapped:", forControlEvents: .TouchUpInside)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
